7a788206afdc5e23d662abff8189109d5b269ce6
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2024-08-04 16:09:59 +0530

    Cancelled listening

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,6 @@ app.use(cors({
 app.use('/api/auth/', require('./routes/auth'))
 app.use('/api/notes/', require('./routes/notes'))
 app.use('/', (req, res)=>{
+    console.log("Running!!!")
     res.send("Hello World")
-})
-
-app.listen(port=>{
-    console.log("Listening to port", 5000)
 })
