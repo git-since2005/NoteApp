910c8ef8eeabf5ae64b8bcd0ead2fac859d3a252
git-since2005 <84366921+git-since2005@users.noreply.github.com>
2024-08-12 18:15:35 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {React, Suspense, lazy} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+const logo = lazy(()=>import('./Images/loading.png'))
 const SignIn = lazy(()=>import('./Components/SignIn'))
 const Signup = lazy(()=>import('./Components/Signup'))
 const Noteboard = lazy(()=>import('./Components/Noteboard.js'))
@@ -9,9 +10,9 @@ function App() {
     <>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Suspense fallback={<p>Loading...</p>}><Signup /></Suspense>} />
-              <Route path="/signin" element = {<Suspense fallback={<p>Loading...</p>}><SignIn /></Suspense>} />
-            <Route path="/notes" element = {<Suspense fallback={<p>Loading...</p>}><Noteboard /></Suspense>} />
+            <Route path="/" element={<Suspense fallback={<p style={{"marginTop":"25%","marginLeft":"50%",'transform':'translate(-50px, -50px)'}}>Loading...</p>}><Signup /></Suspense>} />
+              <Route path="/signin" element = {<Suspense fallback={<p style={{"marginTop":"25%","marginLeft":"50%",'transform':'translate(-50px, -50px)'}}>Loading...</p>}><SignIn /></Suspense>} />
+            <Route path="/notes" element = {<Suspense fallback={<p style={{"marginTop":"25%","marginLeft":"50%",'transform':'translate(-50px, -50px)'}}>Fetching Details...</p>}><Noteboard /></Suspense>} />
           </Routes>
         </BrowserRouter>
     </>
